9104bfbb9f81ee3182c658fc9dbd5c8d72cf1146
Harshil Goel <harshil@hypermode.com>
2025-10-21 05:15:06 +0530

    fixed buid

---
+++ third_party/libneo4j-omni/config.h
@@ -0,0 +1,235 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define if building universal (internal helper macro) */
+/* #undef AC_APPLE_UNIVERSAL_BUILD */
+
+/* Define to 1 if you have AI_ADDRCONFIG in the <netdb.h> header file. */
+#define HAVE_AI_ADDRCONFIG 1
+
+/* Define to 1 if you have the `ASN1_STRING_get0_data' function. */
+#define HAVE_ASN1_STRING_GET0_DATA 1
+
+/* Define to 1 if you have be64toh. */
+#define HAVE_BE64TOH 1
+
+/* Define to 1 if you have the `BIO_meth_new' function. */
+#define HAVE_BIO_METH_NEW 1
+
+/* Define to 1 if you have the <bsd/readpassphrase.h> header file. */
+#define HAVE_BSD_READPASSPHRASE_H 1
+
+/* Define to 1 if you have bswap_64. */
+/* #undef HAVE_BSWAP_64 */
+
+/* Define to 1 if you have the `CRYPTO_set_locking_callback' function. */
+/* #undef HAVE_CRYPTO_SET_LOCKING_CALLBACK */
+
+/* Define to 1 if C supports variable-length arrays. */
+#define HAVE_C_VARARRAYS 1
+
+/* Define to 1 if you have the declaration of `strerror_r', and to 0 if you
+   don't. */
+#define HAVE_DECL_STRERROR_R 1
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Define to 1 if you have the <endian.h> header file. */
+#define HAVE_ENDIAN_H 1
+
+/* Define to 1 if you have htobe64. */
+#define HAVE_HTOBE64 1
+
+/* Define to 1 if you have htonll. */
+/* #undef HAVE_HTONLL */
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define if you have <langinfo.h> and nl_langinfo(CODESET). */
+#define HAVE_LANGINFO_CODESET 1
+
+/* Define to 1 if you have the <libkern/OSByteOrder.h> header file. */
+/* #undef HAVE_LIBKERN_OSBYTEORDER_H */
+
+/* Define to 1 if you have the `memset_s' function. */
+/* #undef HAVE_MEMSET_S */
+
+/* Define to 1 if you have the `mkstemp' function. */
+#define HAVE_MKSTEMP 1
+
+/* Define to 1 if you have MSG_NOSIGNAL in the <sys/socket.h> header file. */
+#define HAVE_MSG_NOSIGNAL 1
+
+/* Define to 1 if you have ntohll. */
+/* #undef HAVE_NTOHLL */
+
+/* Define to 1 if you have openssl. */
+#define HAVE_OPENSSL 1
+
+/* Define to 1 if you have the `open_memstream' function. */
+#define HAVE_OPEN_MEMSTREAM 1
+
+/* Define to 1 if you have OSSwapBigToHostInt64 in the <libkern/OSByteOrder.h>
+   header file. */
+/* #undef HAVE_OSSWAPBIGTOHOSTINT64 */
+
+/* Define to 1 if you have OSSwapHostToBigInt64 in the <libkern/OSByteOrder.h>
+   header file. */
+/* #undef HAVE_OSSWAPHOSTTOBIGINT64 */
+
+/* Define to 1 if you have pthreads. */
+#define HAVE_PTHREADS 1
+
+/* Have PTHREAD_PRIO_INHERIT. */
+#define HAVE_PTHREAD_PRIO_INHERIT 1
+
+/* Define to 1 if you have the `readpassphrase' function. */
+#define HAVE_READPASSPHRASE 1
+
+/* Define to 1 if you have the <readpassphrase.h> header file. */
+/* #undef HAVE_READPASSPHRASE_H */
+
+/* Define to 1 if you have SO_NOSIGPIPE in the <sys/socket.h> header file. */
+/* #undef HAVE_SO_NOSIGPIPE */
+
+/* Define to 1 if you have the <stdatomic.h> header file. */
+#define HAVE_STDATOMIC_H 1
+
+/* Define to 1 if stdbool.h conforms to C99. */
+#define HAVE_STDBOOL_H 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdio.h> header file. */
+#define HAVE_STDIO_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define if you have `strerror_r'. */
+#define HAVE_STRERROR_R 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/endian.h> header file. */
+/* #undef HAVE_SYS_ENDIAN_H */
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if tls support is enabled. */
+#define HAVE_TLS 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to 1 if the system has the type `_Bool'. */
+#define HAVE__BOOL 1
+
+/* Define to the sub-directory where libtool stores uninstalled libraries. */
+#define LT_OBJDIR ".libs/"
+
+/* Define to 1 to disable assertions. */
+/* #undef NDEBUG */
+
+/* Define to 1 to disable thread local IO vectors. */
+/* #undef NO_THREAD_LOCAL_IOV */
+
+/* Name of package */
+#define PACKAGE "libneo4j-client"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT ""
+
+/* Define to the development version of this package */
+#define PACKAGE_DEVELOPMENT_VERSION ""
+
+/* Define to the major version of this package */
+#define PACKAGE_MAJOR_VERSION 5
+
+/* Define to the minor version of this package */
+#define PACKAGE_MINOR_VERSION 0
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "libneo4j-client"
+
+/* Define to the patch version of this package */
+#define PACKAGE_PATCH_VERSION 7
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "libneo4j-client 5.0.7"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "libneo4j-client"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "5.0.7"
+
+/* Define to necessary symbol if this constant uses a non-standard name on
+   your system. */
+/* #undef PTHREAD_CREATE_JOINABLE */
+
+/* Define to 1 if all of the C90 standard headers exist (not just the ones
+   required in a freestanding environment). This macro is provided for
+   backward compatibility; new code need not use it. */
+#define STDC_HEADERS 1
+
+/* Define to 1 if strerror_r returns char *. */
+/* #undef STRERROR_R_CHAR_P */
+
+/* If the compiler supports a thread local storage class define it to that
+   here */
+#define THREAD_LOCAL __thread
+
+/* Version number of package */
+#define VERSION "5.0.7"
+
+/* Define WORDS_BIGENDIAN to 1 if your processor stores words with the most
+   significant byte first (like Motorola and SPARC, unlike Intel). */
+#if defined AC_APPLE_UNIVERSAL_BUILD
+# if defined __BIG_ENDIAN__
+#  define WORDS_BIGENDIAN 1
+# endif
+#else
+# ifndef WORDS_BIGENDIAN
+/* #  undef WORDS_BIGENDIAN */
+# endif
+#endif
+
+/* Define to 1 if C does not support variable-length arrays, and if the
+   compiler does not already define this. */
+/* #undef __STDC_NO_VLA__ */
+
+/* Define to empty if `const' does not conform to ANSI C. */
+/* #undef const */
+
+/* Define to `__inline__' or `__inline' if that's what the C compiler
+   calls it, or to nothing if 'inline' is not supported under any name.  */
+#ifndef __cplusplus
+/* #undef inline */
+#endif
+
+/* Define to `unsigned int' if <sys/types.h> does not define. */
+/* #undef size_t */
+
+/* Substitute for socklen_t. */
+/* #undef socklen_t */
+
+/* Define to `int' if <sys/types.h> does not define. */
+/* #undef ssize_t */
+
+/* Substitute for static_assert */
+/* #undef static_assert */
